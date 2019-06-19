3f06489802484310a9b1a6682b351c47e4720de8
kingchika <kingmrprogrammer@gmail.com>
2019-06-19 06:20:51 +0100

    Update CircularRotateArray.java
    i updated this for efficiency reasons

--- src/main/java/com/the3linecard/project/otherClasses/CircularRotateArray.java
+++ src/main/java/com/the3linecard/project/otherClasses/CircularRotateArray.java
@@ -2,12 +2,12 @@ package com.the3linecard.project.otherClasses;
 
 public class CircularRotateArray {
 	int[] data = {2, 3, 4, 5, 6};
-	int[] temp = new int[5];
 	int numberOfRotation = 3;
 	
 	public CircularRotateArray() {
-		
+	  /*	
 		for(int i= 1; i <= numberOfRotation; i++) {
+		   int[] temp = new int[5];
 			if(i == 1) {
 			temp[0] = data[4];
 			temp[1] = data[0];
@@ -29,14 +29,37 @@ public class CircularRotateArray {
 				temp[3] = data[0];
 				temp[4] = data[1];
 			}
-		}
+		}	
 		//copies the value of temporary arrary to the main data
 		data = temp;
 		  for(int i = 0; i < data.length; i++) {
-			  System.out.println(data[i]+',');
+			  System.out.println(data[i]+",");
 		  }
 		  
-		 
+		 */
+	
+		// I have updated it for efficiecy purposes
+		// i did the first one because i developing the REST API so i just did it to finish my API
+		// That is the reason why i had to modify it now
+		//Thanks
+		        
+		for(int i= 1; i <= numberOfRotation; i++) {
+                    int[] temp = new int[5];
+                    
+                     for(int j = 0; j < data.length; j++){
+                         if(j == 0){
+                             temp[j] = data[data.length - 1];
+                         }
+                         else{
+                             temp[j] = data[j - 1];
+                         }
+                     }
+                           data = temp;
+		}
+		
+		  for(int i = 0; i < data.length; i++) {
+			  System.out.println(data[i]+",");
+		  }
 	}
 	
 }
