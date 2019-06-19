08576b7d7860dc2c22946757c4ec2b03c044e7da
kingchika <kingmrprogrammer@gmail.com>
2019-06-19 04:32:59 +0100

    Update StudentRepository.java

--- src/main/java/com/the3linecard/project/repository/StudentRepository.java
+++ src/main/java/com/the3linecard/project/repository/StudentRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.the3linecard.project.entities.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+	@Transactional
 	@Modifying
 	@Query("UPDATE Student s SET s.studentClass = ?2, s.age = ?3 WHERE s.id = ?1")
 	public void updateAgeAndClass(Long id, String stdClass, int age);
